72d62967e2809819a28f88984754863e96defbf5
unknown <celalcanh.h.oglu@gmail.com>
2022-05-13 13:17:18 +0300

    güncelleme

--- style.js
+++ style.js
@@ -1,4 +1,4 @@
-let opacity = 0;
+/* let opacity = 0;
 let click=1;
 
 let kayma=0;
@@ -16,9 +16,9 @@ function t(){
         document.getElementById('about').style.opacity = opacity;
       
      }
-}
+} */
 
-function y(){
+/* function y(){
     kayma=0;
     kaymaf();
     function kaymaf(){
@@ -26,14 +26,50 @@ function y(){
         if(kayma<40){
         setTimeout(function(){kaymaf()},8);
              
-         }
+         } */
          /* else {
             kayma=0;
                      } */
-         document.getElementById('about').style.transform = "translateY(-"+kayma+"px)";
+      /*    document.getElementById('about').style.transform = "translateY(-"+kayma+"px)";
        
     }
     
+} */
+
+
+/* document.getElementById("about").onmouseover = function() {mouseOver()};
+document.getElementById("about").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+   
+    MyFadeFunction();
+    function MyFadeFunction() {
+
+        if (opacity<1  ) {
+           opacity += .07;
+           
+           setTimeout(function(){MyFadeFunction()},18);
+        }
+        document.getElementById("about").style.backgroundColor = "rgb(34, 207, 213,"+opacity+" )";
+      
+     }
+    
+    
 }
 
+function mouseOut() {
+  
+    
+    MyFadeFunction();
+    function MyFadeFunction() {
 
+        if (opacity>0  ) {
+           opacity -= .07;
+           
+           setTimeout(function(){MyFadeFunction()},18);
+        }
+        document.getElementById("about").style.backgroundColor =  "rgb(34, 207, 213,"+opacity+" )";
+      
+     }
+}
+ */
